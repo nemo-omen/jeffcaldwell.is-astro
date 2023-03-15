a2652bc9beb488530819a1a5d246d79cb3b78517
nemo-omen <nemo.omen@gmail.com>
2023-03-15 11:57:12 -0500

    Switch to static, use Picture component, set image sizes

---
+++ .vercel/output/functions/render.func/src/content/projects/one.md
@@ -0,0 +1,11 @@
+---
+title: Test Project One
+summary: Nulla vitae elit libero, a pharetra augue. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Maecenas faucibus mollis interdum. Donec sed odio dui.
+date: 2023-03-23
+image:
+  src: writingfeature
+  alt: A nice picture of a grackle
+draft: true
+---
+
+Okay, so here's some content.
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,26 @@
 import { defineConfig } from "astro/config";
 import tokyonight from "./tokyonight.json";
 import mdx from "@astrojs/mdx";
+import image from "@astrojs/image";
+
+// import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  integrations: [mdx()],
-  experimental: {
-    assets: true,
-  },
+  // output: "server",
+  integrations: [
+    mdx(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+  ],
+  // experimental: {
+  //   assets: true,
+  // },
   markdown: {
     shikiConfig: {
       theme: tokyonight,
     },
   },
+  // adapter: vercel()
 });
