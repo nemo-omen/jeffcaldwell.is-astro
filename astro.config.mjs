e5e555fe11f0666d39c71eb124f0ac18b85871a2
nemo-omen <nemo.omen@gmail.com>
2023-03-15 21:06:07 -0500

    Finish footer

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,25 +2,28 @@ import { defineConfig } from "astro/config";
 import tokyonight from "./tokyonight.json";
 import mdx from "@astrojs/mdx";
 import image from "@astrojs/image";
+import sitemap from "@astrojs/sitemap";
 
 // import vercel from "@astrojs/vercel/serverless";
-
-import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   // output: "server",
-  site: "https://jeffcaldwell.is",
-  integrations: [mdx(), image({
-    serviceEntryPoint: "@astrojs/image/sharp"
-  }), sitemap()],
+  // site: "https://jeffcaldwell.is",
+  integrations: [
+    mdx(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+    sitemap(),
+  ],
   // experimental: {
   //   assets: true,
   // },
   markdown: {
     shikiConfig: {
-      theme: tokyonight
-    }
-  }
+      theme: tokyonight,
+    },
+  },
   // adapter: vercel()
 });
